4319579dd366d61dd7593e414ce31c1daf76807f
manh130399 <manhnguyen130399@gmail.com>
2019-07-05 17:08:26 +0700

    up

--- QLHSAdmin/src/views/menutab/tabthongke/ThongKeController.java
+++ QLHSAdmin/src/views/menutab/tabthongke/ThongKeController.java
@@ -13,8 +13,17 @@ import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.print.PageLayout;
+import javafx.print.PageOrientation;
+import javafx.print.Paper;
+import javafx.print.PrintResolution;
+import javafx.print.Printer;
+import javafx.print.PrinterJob;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.transform.Scale;
 
 public class ThongKeController implements Initializable {
 
@@ -22,12 +31,53 @@ public class ThongKeController implements Initializable {
     private BorderPane borderpaneRoot;
     @FXML
     private ScrollPane scrollpaneContent;
+    @FXML
+    private  Button buttonUp;
+    @FXML
+    private Button buttonPrint;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
             FXMLLoader fxmll = new FXMLLoader(this.getClass().getResource("TKContentForm.fxml"));
             this.scrollpaneContent.setContent(fxmll.load());
+             this.buttonUp.setOnAction((event) -> {
+                this.scrollpaneContent.setVvalue(0);
+                this.scrollpaneContent.setHvalue(0);
+            });
+
+            this.buttonPrint.setOnAction((event) -> {
+                VBox vBoxContent = (VBox) this.scrollpaneContent.getContent();
+
+                PrinterJob job = PrinterJob.createPrinterJob();
+
+                if (job != null && job.showPrintDialog(vBoxContent.getScene().getWindow())) {
+                    Printer printer = job.getPrinter();
+                    PageLayout pageLayout = printer.createPageLayout(Paper.A3, PageOrientation.PORTRAIT, Printer.MarginType.EQUAL);
+
+                    double width = vBoxContent.getWidth();
+                    double height = vBoxContent.getHeight();
+
+                    PrintResolution resolution = job.getJobSettings().getPrintResolution();
+
+                    width /= resolution.getFeedResolution();
+
+                    height /= resolution.getCrossFeedResolution();
+
+                    double scaleX = pageLayout.getPrintableWidth() / width / 600;
+                    double scaleY = pageLayout.getPrintableHeight() / height / 600;
+
+                    Scale scale = new Scale(scaleX, scaleY);
+
+                    vBoxContent.getTransforms().add(scale);
+
+                    boolean success = job.printPage(pageLayout, vBoxContent);
+                    if (success) {
+                        job.endJob();
+                    }
+                    vBoxContent.getTransforms().remove(scale);
+                }
+            });
         } catch (IOException ex) {
             Logger.getLogger(ThongKeController.class.getName()).log(Level.SEVERE, null, ex);
         }        
